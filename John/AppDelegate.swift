a89d016217cd31f41c88ba9618f81098f09ce9cb
Chris Svenningsen <chris@carbonfive.com>
2015-02-06 09:29:57 -0800

    Initial commit.

---
+++ John/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  John
+//
+//  Created by Chris Svenningsen on 2/6/15.
+//  Copyright (c) 2015 Carbon Five. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
